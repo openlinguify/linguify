3d8379f38e55459aeca3b2f3db29acd55f45f163
Louis-Philippe <louisphilippelalou@outlook.com>
2024-05-16 23:30:36 +0200

    Some changes in the chart app

--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,9 @@
 # cards/views.py
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
+    CreateView,
+    UpdateView,
 )
 from .models import Card
 
@@ -9,3 +12,10 @@ class CardListView(ListView):
     model = Card
     queryset = Card.objects.all().order_by("box", "-date_created")
 
+class CardCreateView(CreateView):
+    model = Card
+    fields = ["question", "answer", "box"]
+    success_url = reverse_lazy("card-create")
+
+class CardUpdateView(CardCreateView, UpdateView):
+    success_url = reverse_lazy("card_list")
