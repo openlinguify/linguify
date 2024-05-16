7ca9150c314b75ea0b34db34b69773f17eba44bb
Louis-Philippe <louisphilippelalou@outlook.com>
2024-05-16 21:44:07 +0200

    Some change tests brought to the project

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+
 class SignupForm (UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ['username', 'email', 'first_name', 'last_name', 'role', 'profile_picture', 'mother_language', 'learning_language', 'objectives',
-                  'level']
-
-
+        fields = ['username', 'email', 'first_name', 'last_name',
+                  'role', 'profile_picture', 'mother_language',
+                  'learning_language', 'objectives', 'level']
