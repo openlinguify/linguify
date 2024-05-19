4c62313f05413e91418aef430e65dede32e2d382
Louis-Philippe <louisphilippelalou@outlook.com>
2024-05-19 19:33:35 +0200

    chore: Update Django generated files
    This commit updates the Django generated files in the `cards` and `linguify` apps. The `0001_initial.py` files were regenerated by Django, and the only change is the timestamp in the file header. This update ensures that the generated files reflect the correct timestamp.
    
    The commit message suggests that the changes are related to updating Django generated files in the `cards` and `linguify` apps.

--- lingoProject/urls.py
+++ lingoProject/urls.py
@@ -6,23 +6,18 @@ from django.contrib.auth.views import (
 )
 from authentication import views as auth_views
 from linguify import views as linguify_views
+from django.conf import settings
+from django.conf.urls.static import static  # Add this import
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', linguify_views.base),
-
-    path('', LoginView.as_view(
-        template_name='authentication/login.html',
-        redirect_authenticated_user=False
-    ), name='login'),
+    path('', LoginView.as_view(template_name='authentication/login.html', redirect_authenticated_user=False), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', auth_views.signup_page, name='signup'),
-    path('change-password/', PasswordChangeView.as_view(
-        template_name='authentication/password_change_form.html'
-    ), name='password_change'),
-    path('change-password-done/', PasswordChangeDoneView.as_view(
-        template_name='authentication/password_change_done.html'
-    ), name='password_change_done'),
+    path('change-password/', PasswordChangeView.as_view(template_name='authentication/password_change_form.html'), name='password_change'),
+    path('change-password-done/', PasswordChangeDoneView.as_view(template_name='authentication/password_change_done.html'), name='password_change_done'),
     path('home/', linguify_views.home, name='home'),
     path('index/', linguify_views.index, name='index'),
     path('vocabulaire/', linguify_views.vocabulaire, name='vocabulaire'),
@@ -31,9 +26,7 @@ urlpatterns = [
     path('revision/', linguify_views.revision, name='revision'),
     path('quiz/', linguify_views.quiz, name='quiz'),
     path('pricing/', linguify_views.prices, name='pricing'),
-
     path('testlinguisitique/', linguify_views.testlinguisitique, name='testlinguisitique'),
-
     path('result/', linguify_views.check_answer, name='result'),
     path('header/', linguify_views.header),
     path('footer/', linguify_views.footer),
@@ -43,3 +36,7 @@ urlpatterns = [
     path('about/', linguify_views.about, name='about'),
     path("", include("cards.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
