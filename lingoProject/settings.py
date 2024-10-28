31fe8aa0615106b2084c5fa7083b24e1ac623d1f
Linguify <louisphilippelalou@outlook.com>
2024-10-28 23:04:15 +0100

    postgre

--- lingoProject/settings.py
+++ lingoProject/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'lingoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_linguify',
+        'USER': 'postgres',
+        'PASSWORD': 'azerty',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
