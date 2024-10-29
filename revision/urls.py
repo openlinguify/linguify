9c0be1f40382b37045bf3b5fda2c6387091fd3b9
Linguify <louisphilippelalou@outlook.com>
2024-10-29 20:22:32 +0100

    Design revision page - add-revision

--- revision/urls.py
+++ revision/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.revision, name='revision'),
-    path('add/', views.add_revision, name='add_revision'),
+    path('add-revision/', views.add_revision, name='add_revision'),
     path('list/', views.reviewed_vocabulaire_list, name='reviewed_vocabulaire_list'),
     path('edit/<int:revision_id>/', views.edit_revision, name='edit_revision'),
     path('delete/<int:revision_id>/', views.delete_revision, name='delete_revision'),
