db8aa421242854813cd933c467f4a7c8397f5d70
Louis-Philippe Lalou <louisphilippelalou@outlook.com>
2024-11-07 22:15:37 +0100

    reset npm

---
+++ frontend/src/components/ThemeList.js
@@ -0,0 +1,30 @@
+// src/components/ThemeList.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+function ThemeList() {
+  const [themes, setThemes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/themes/')
+      .then(response => setThemes(response.data))
+      .catch(error => console.error('Erreur:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Liste des Thèmes</h1>
+      <ul>
+        {themes.map(theme => (
+          <li key={theme.id}>
+            <Link to={`/theme/${theme.id}`}>{theme.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ThemeList;
+
