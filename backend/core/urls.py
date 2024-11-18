eb739f4925b670295ca6a818d5ce530966c3a517
Louis-Philippe Lalou <louisphilippelalou@outlook.com>
2024-11-18 02:00:41 +0100

    api authentication done for User

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,18 @@
-# backend/core/urls.py
+# core/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
+from django_apps.routers import router
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('django_apps.authentication.urls')),
+    path('api/', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
