43cfbadb0d0018d3f986d1eb9c61f09ec7012c96
Louis-Philippe Lalou <louisphilippelalou@outlook.com>
2024-11-25 23:36:58 +0100

    this line router was delete due to the suppression of the routers file in the previous django_apps file grouping all the apps

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,7 +2,6 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from django_apps.routers import router
 from django.conf import settings
 from django.conf.urls.static import static
 
