2c4d9de395cf6da75a4de2c0be1fa5f6b9e02eb4
Louis-Philippe Lalou <louisphilippelalou@outlook.com>
2024-12-25 12:32:44 +0100

    clerk authentication install
    Signed-off-by: Louis-Philippe Lalou <louisphilippelalou@outlook.com>

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,6 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// frontend/middleware.ts
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// Crée un vérificateur pour les routes publiques
+const isPublicRoute = createRouteMatcher(["/test"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  // Récupérer l'authentification de l'utilisateur
+  const { userId } = await auth(); // Résout la promesse
+
+  // Si ce n'est pas une route publique et qu'il n'y a pas d'utilisateur connecté, renvoie une erreur 401
+  if (!isPublicRoute(req) && !userId) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+});
 
 export const config = {
   matcher: [
