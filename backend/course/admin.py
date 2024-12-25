b9e39018c71ad9e6c66d42f489f0d1195dff3d2a
Louis-Philippe Lalou <louisphilippelalou@outlook.com>
2024-12-26 00:42:06 +0100

    import data vocabulary List + Unit testing phase
    Signed-off-by: Louis-Philippe Lalou <louisphilippelalou@outlook.com>

--- backend/course/admin.py
+++ backend/course/admin.py
@@ -1,46 +1,8 @@
 # course/admin.py
 from django.contrib import admin
-from .models import (
-    Language,
-    Level,
-    LearningPath,
-    Unit,
-    Lesson,
-    VocabularyList,
-    ExerciseVocabularyMultipleChoice,
-    ExerciseVocabularyFillBlank,
-    ExerciseGrammarReordering,
-    Exercise,
-    GrammarRule,
-    Grammar,
-    GrammarRulePoint,
-    Reading,
-    Writing,
-    TestRecap,
-    TestRecapExercise,
-    TestRecapAttempt,
-    GrammarModule,
-    GrammarRuleContent,
-    GrammarRuleExercise,
-)
+from .models import Unit, Lesson, VocabularyList, ExerciseVocabularyMultipleChoice
 
-admin.site.register(Language)
-admin.site.register(Level)
-admin.site.register(LearningPath)
 admin.site.register(Unit)
 admin.site.register(Lesson)
 admin.site.register(VocabularyList)
 admin.site.register(ExerciseVocabularyMultipleChoice)
-admin.site.register(ExerciseVocabularyFillBlank)
-admin.site.register(ExerciseGrammarReordering)
-admin.site.register(Exercise)
-admin.site.register(Grammar)
-admin.site.register(GrammarRulePoint)
-admin.site.register(Reading)
-admin.site.register(Writing)
-admin.site.register(TestRecap)
-admin.site.register(TestRecapExercise)
-admin.site.register(TestRecapAttempt)
-admin.site.register(GrammarModule)
-admin.site.register(GrammarRuleContent)
-admin.site.register(GrammarRuleExercise)
--- backend/course/urls.py
+++ backend/course/urls.py
@@ -1,9 +1,6 @@
 # backend/course/urls.py
 from django.urls import path
 from .views import (
-    LanguageListView,
-    LevelListView,
-    LearningPathAPIView,
     UnitAPIView,
     LessonAPIView,
     ExerciceVocabularyAPIView,
@@ -13,9 +10,6 @@ from .views import (
 app_name = 'course'
 
 urlpatterns = [
-    path('languages/', LanguageListView.as_view(), name='api-language'),
-    path('levels/', LevelListView.as_view(), name='api-level'),
-    path('learning-path/<int:pk>/', LearningPathAPIView.as_view(), name='api-learning-path'),
     path('units/', UnitAPIView.as_view(), name='unit-list'),
     path('lesson/', LessonAPIView.as_view(), name='api-lesson'),
     path('exercice-vocabulary/', ExerciceVocabularyAPIView.as_view(), name='api-exercice-vocabulary'),
