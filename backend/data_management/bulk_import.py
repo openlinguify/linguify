54fcdeec80906a00c22aab47a5ed232cfcdbda14
Louis-Philippe Lalou <louisphilippelalou@outlook.com>
2024-12-26 19:50:33 +0100

    save
    Signed-off-by: Louis-Philippe Lalou <louisphilippelalou@outlook.com>

--- backend/data_management/bulk_import.py
+++ backend/data_management/bulk_import.py
@@ -1,5 +1,16 @@
+import os
 import csv
-from course.models import Unit  # Remplacez par votre application et modèle
+from course.models import Unit, VocabularyList  # Remplacez par votre application et modèle
+
+
+def validate_file_path(file_path: str) -> None:
+    """Vérifie si le fichier existe et est accessible"""
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Le fichier '{file_path}' n'existe pas.")
+    if not os.path.isfile(file_path):
+        raise ValueError(f"'{file_path}' n'est pas un fichier.")
+    if not file_path.endswith('.csv'):
+        raise ValueError(f"'{file_path}' n'est pas un fichier CSV.")
 
 def import_units_from_csv(file_path):
     with open(file_path, encoding='utf-8') as csv_file:
@@ -31,5 +42,39 @@ file_path = "C:/Users/louis/OneDrive/Bureau/content/list_unit.csv"
 import_units_from_csv(file_path)
 
 
+def import_vocabulary_lists_from_csv(file_path):
+    with open(file_path, encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+        vocabulary_lists = []
 
+        for row in reader:
+            vocabulary_list = VocabularyList(
+
+                lesson_id=int(row['lesson_id']),
+                word_en=row['word_en'],
+                word_fr=row['word_fr'],
+                word_es=row['word_es'],
+                word_nl=row['word_nl'],
+                definition_en=row['definition_en'],
+                definition_fr=row['definition_fr'],
+                definition_es=row['definition_es'],
+                definition_nl=row['definition_nl'],
+                example_sentence_en=row['example_sentence_en'],
+                example_sentence_fr=row['example_sentence_fr'],
+                example_sentence_es=row['example_sentence_es'],
+                example_sentence_nl=row['example_sentence_nl'],
+                word_type_en=row['word_type_en'],
+                word_type_fr=row['word_type_fr'],
+                word_type_es=row['word_type_es'],
+                word_type_nl=row['word_type_nl']
+            )
+            vocabulary_lists.append(vocabulary_list)
+        
+        # Insérer massivement avec bulk_create pour optimiser
+        VocabularyList.objects.bulk_create(vocabulary_lists)
+        print(f"{len(vocabulary_lists)} listes de vocabulaire ajoutées avec succès.")
+
+# Spécifiez le chemin de votre fichier CSV
+file_path = "C:/Users/louis/OneDrive/Bureau/content/vocabulary_list.csv"
+import_vocabulary_lists_from_csv(file_path)
 
