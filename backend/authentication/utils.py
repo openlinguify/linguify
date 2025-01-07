ad4f4d335e9ab37df3e09304f5aafcebfee1a684
Lionel H <me@nullbyte.be>
2025-01-07 16:27:30 +0100

    feat: add auth0 is the backend
    TO BE TESTED with the frontend
    
    but the django integration in the backend is done.
    
    Signed-off-by: Lionel H <me@nullbyte.be>

--- backend/authentication/utils.py
+++ backend/authentication/utils.py
@@ -1,6 +1,13 @@
 # backend/django_apps/authentication/utils.py
 
+import json
+
+import jwt
+import requests
+
+from django.contrib.auth import authenticate
 from django.core.mail import send_mail
+
 
 def notify_coach_of_commission_change(coach_profile):
     """
@@ -25,3 +32,23 @@ def notify_coach_of_commission_change(coach_profile):
         "The Platform Team"
     )
     send_mail(subject, message, 'noreply@platform.com', [coach_email])
+
+def jwt_get_username_from_payload_handler(payload):
+    username = payload.get('sub').replace('|', '.')
+    authenticate(remote_user=username)
+    return username
+
+
+def jwt_decode_token(token):
+    header = jwt.get_unverified_header(token)
+    jwks = requests.get('https://{}/.well-known/jwks.json'.format('dev-hazi5dwwkk7pe476.eu.auth0.com')).json()
+    public_key = None
+    for jwk in jwks['keys']:
+        if jwk['kid'] == header['kid']:
+            public_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
+
+    if public_key is None:
+        raise Exception('Public key not found.')
+
+    issuer = 'https://{}/'.format('dev-hazi5dwwkk7pe476.eu.auth0.com')
+    return jwt.decode(token, public_key, audience='https://dev-hazi5dwwkk7pe476.eu.auth0.com/api/v2/', issuer=issuer, algorithms=['RS256'])
