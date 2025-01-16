867f5296f0edfa48c1bfbc07dc73d59ccace9cab
Louis-Philippe Lalou <louisphilippelalou@outlook.com>
2025-01-16 21:20:01 +0100

    Auth0 Done + Sign in button
    Signed-off-by: Louis-Philippe Lalou <louisphilippelalou@outlook.com>

--- frontend/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,6 +0,0 @@
-// src/app/(dashboard)/_components/settings.tsx
-import { SignIn } from '@clerk/nextjs'
-
-export default function Page() {
-  return <SignIn />
-}
--- frontend/src/app/api/auth/[auth0]/route.ts
+++ frontend/src/app/api/auth/[auth0]/route.ts
@@ -2,6 +2,3 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
 const handler = handleAuth();
-
-export const GET = handler;
-export const POST = handler;
