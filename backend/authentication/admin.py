58f32700ed8306948acd1d5da4d1a17c0e81ce4b
Louis-Philippe Lalou <louisphilippelalou@outlook.com>
2025-01-28 14:49:43 +0100

    auth cleaning
    Signed-off-by: Louis-Philippe Lalou <louisphilippelalou@outlook.com>

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -3,6 +3,3 @@ from .models import User, CoachProfile, Review, UserFeedback
 # Register your models here.
 
 admin.site.register(User)
-admin.site.register(CoachProfile)
-admin.site.register(Review)
-admin.site.register(UserFeedback)
