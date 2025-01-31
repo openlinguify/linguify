bd34fe63cde28152dfde58e25432de95fb79228c
Louis-Philippe Lalou <louisphilippelalou@outlook.com>
2025-01-31 21:29:33 +0100

    update the revision frontend
    Signed-off-by: Louis-Philippe Lalou <louisphilippelalou@outlook.com>

--- frontend/src/app/(dashboard)/(apps)/revision/layout.tsx
+++ frontend/src/app/(dashboard)/(apps)/revision/layout.tsx
@@ -1,12 +1,25 @@
 // src/app/(apps)/revision/layout.tsx
-export default function RevisionLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <div className="flex min-h-screen flex-col">
-        <div className="flex-1">{children}</div>
-      </div>
-    );
-  }
+// src/app/layout.tsx
+import { Inter } from 'next/font/google';
+import QueryProvider from '@/providers/QueryProvider';
+import { Toaster } from '@/components/ui/toaster';
+import 'tailwindcss/tailwind.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <QueryProvider>
+          {children}
+          <Toaster />
+        </QueryProvider>
+      </body>
+    </html>
+  );
+}
