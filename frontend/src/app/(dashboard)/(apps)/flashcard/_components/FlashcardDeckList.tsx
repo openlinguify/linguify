64bfdfba1b1bf41974c0db188866419bba866541
Louis-Philippe Lalou <louisphilippelalou@outlook.com>
2025-03-04 21:01:47 +0100

    save

--- frontend/src/app/(dashboard)/(apps)/flashcard/_components/FlashcardDeckList.tsx
+++ frontend/src/app/(dashboard)/(apps)/flashcard/_components/FlashcardDeckList.tsx
@@ -1,3 +1,5 @@
+// src/app/dashboard/apps/flashcard/_components/FlashcardDeckList.tsx
+'use client';
 import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
